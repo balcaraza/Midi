6118b3531fe92a89614d3d54b45e1ca563efa5fd
Balam <balcaraza@gmail.com>
2025-08-22 18:16:16 -0600

    Feature | funcion tecla pom

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-document.querySelector('.tecla_pom');
+function playSonidoPom(){
+    document.querySelector('#sonido_tecla_pom').play();
+}
+
+function playSonidoClap(){
+    document.querySelector('#sonido_tecla_clap').play();
+}
+
+document.querySelector('.tecla_pom').onclick = playSonidoPom;
+
+
